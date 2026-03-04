a02d65820f6d97139d16ff2b4d141ce4dd2d4569
Shahd Qaddoura <shahd.kaddoura182@gmail.com>
2026-03-04 23:30:01 +0200

    Added the scores mockdata

---
+++ src/data/scores.js
@@ -0,0 +1,8 @@
+export const scores=[
+    {id:1, matchId:1, teamAScore:25, teamBScore:23},
+    {id:2, matchId:2, teamAScore:26, teamBScore:24},
+    {id:3, matchId:3, teamAScore:25, teamBScore:12},
+    {id:4, matchId:4, teamAScore:28, teamBScore:26},
+    {id:5, matchId:5, teamAScore:25, teamBScore:21},
+    {id:6, matchId:6, teamAScore:23, teamBScore:25}
+];
