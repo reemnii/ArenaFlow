e0ef27b75b6c532f4a87df5eeac5c89b9ff2854b
Shahd Qaddoura <shahd.kaddoura182@gmail.com>
2026-03-04 23:30:42 +0200

    Added the standings mockdata

---
+++ src/data/standings.js
@@ -0,0 +1,8 @@
+export const standings=[
+    {id:1, tournamentId:1, teamId:1, wins:1, losses:1, points:3},
+    {id:2, tournamentId:1, teamId:2, wins:2, losses:0, points:6},
+    {id:3, tournamentId:1, teamId:3, wins:0, losses:2, points:0},
+    {id:4, tournamentId:2, teamId:4, wins:1, losses:0, points:3},
+    {id:5, tournamentId:2, teamId:5, wins:0, losses:2, points:0},
+    {id:6, tournamentId:2, teamId:6, wins:1, losses:1, points:3}
+];
