07f4333ec77603a91e795f939f618178b6f15d15
Shahd Qaddoura <shahd.kaddoura182@gmail.com>
2026-03-04 23:25:39 +0200

    Added the teams mockdata

--- src/data/teams.js
+++ src/data/teams.js
@@ -1,5 +1,8 @@
-export const teams = [
-  { id: 1, name: "Spike Masters" },
-  { id: 2, name: "Net Breakers" },
-  { id: 3, name: "Volley Kings" }
+export const teams=[
+    {id:1, name:"Athens Aces Men", gender:"male"},
+    {id:2, name:"Thessaloniki Titans Men", gender:"male"},
+    {id:3, name:"Volleyball Vipers Men", gender:"male"},
+    {id:4, name:"Athens Aces Women", gender:"female"},
+    {id:5, name:"Thessaloniki Titans Women", gender:"female"},
+    {id:6, name:"Volleyball Vipers Women", gender:"female"}
 ];
